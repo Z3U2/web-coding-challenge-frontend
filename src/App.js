07c855f7e13af7e255c32b362b6e7bdeaff6077d
z3u2 <ilyassnasr@gmail.com>
2019-06-30 16:51:08 +0100

    Create project structure

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
 
+import LogIn from './login/component'
+import SignUp from './signup/component'
+import NearMe from './nearme/component'
+import Pref from './pref/component'
 
 class App extends React.Component {
-  
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      user: null,
+    }
+  }
+
+  async login() {
+    this.setState(prevState => ({ user: { email: "test@example.com" } }))
+  }
+
+  async logout() {
+    this.setState(prevState => ({ user: null }))
+  }
+
   render() {
     return (
-      <div>
-        Hello World!
-      </div>
+      <Router>
+        <Route path="/" exact component={NearMe} />
+        <Route path="/prefs/" component={Pref} />
+        <Route path="/login/" component={LogIn} />
+        <Route path="/signup/" component={SignUp} />
+      </Router>
     )
   }
 
---
+++ src/pref/component.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default class Pref extends React.Component {
+    render() {
+        return (
+            <div>
+                Pref Component
+            </div>
+        )
+    }
+}
