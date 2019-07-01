584815ab2eef59ff9ad104f2dba98c6a3295bfaf
z3u2 <ilyassnasr@gmail.com>
2019-07-01 02:06:19 +0100

    Add basic login logic

--- src/login/component.js
+++ src/login/component.js
@@ -1,18 +1,40 @@
 import React from 'react'
 import { Redirect } from 'react-router-dom'
 
+import { getLogin } from '../service'
+
 export default class LogIn extends React.Component {
 
     constructor(props) {
         super(props)
         this.setUser = this.props.setUser
         this.login = this.login.bind(this)
+        this.state = {
+            email : '',
+            password : '',
+            error: '',
+        }
     }
 
     async login() {
-        this.setUser({
-            email: "test@example.com"
-        })
+        try {
+            await getLogin(this.state.email,this.state.password)
+            this.setUser({
+                email: this.state.email
+            })
+        }
+        catch (e) {
+            this.setState({ error: e.message })
+        }
+    }
+
+    handleChange = (e) => {
+        let field = e.target.getAttribute('name')
+        let value = e.target.value
+        this.setState(prevState => {
+            prevState[field] = value
+            return prevState
+        });
     }
 
     render() {
@@ -25,9 +47,16 @@ export default class LogIn extends React.Component {
 
         return (
             <div>
+                <label>Email</label>
+                <input type="text" name="email" value={this.state.email} onChange={this.handleChange} />
+                <br/>
+                <label>Password</label>
+                <input type="password" name="password" value={this.state.password} onChange={this.handleChange} />
+                <br/>
                 <button onClick={this.login}>
                     LogIn
                 </button>
+                {this.state.error && <p>{this.state.error}</p> }
             </div>
         )
     }
