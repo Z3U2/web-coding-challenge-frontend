8075c8fd6be31d97a208e384ad3b926005c6d127
z3u2 <ilyassnasr@gmail.com>
2019-07-01 02:20:55 +0100

    Cleared  the mix between nearme and pref components

--- src/nearme/component.js
+++ src/nearme/component.js
@@ -1,38 +1,10 @@
 import React from 'react'
-import { getPrefs } from '../service'
 
-export default class NearMe extends React.Component {
-
-    constructor(props){
-        super(props)
-
-        this.state = {
-            prefs : null
-        }
-    }
-
-    componentDidMount() {
-        getPrefs().then(
-            prefs => {
-                this.setState({prefs})
-            }
-        )
-    }
-
+export default class Pref extends React.Component {
     render() {
-
-        const prefs = this.state.prefs
-
         return (
             <div>
-                {prefs && 
-                <ul>
-                    {prefs.map(pref => {
-                        return (
-                            <li>{pref.name}</li>
-                        )
-                    })}
-                </ul>}
+                NearMe Component
             </div>
         )
     }
--- src/pref/component.js
+++ src/pref/component.js
@@ -1,10 +1,38 @@
 import React from 'react'
+import { getPrefs } from '../service'
 
-export default class Pref extends React.Component {
+export default class NearMe extends React.Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            prefs: null
+        }
+    }
+
+    componentDidMount() {
+        getPrefs().then(
+            prefs => {
+                this.setState({ prefs })
+            }
+        )
+    }
+
     render() {
+
+        const prefs = this.state.prefs
+
         return (
             <div>
-                Pref Component
+                {prefs &&
+                    <ul>
+                        {prefs.map(pref => {
+                            return (
+                                <li key={pref._id}>{pref.name}</li>
+                            )
+                        })}
+                    </ul>}
             </div>
         )
     }
