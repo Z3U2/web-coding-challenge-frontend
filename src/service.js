e4e37b4c728cc2f602d3ad11e88763eb71ab8fc3
z3u2 <ilyassnasr@gmail.com>
2019-07-01 01:47:46 +0100

    Refactor service for error handling

--- src/service.js
+++ src/service.js
@@ -5,8 +5,22 @@ if(env === "development") {
     URL = "http://localhost:4127"
 }
 
+function handleResponse(response) {
+    if (response.status === 200) {
+        if (response.data) return response.data
+        else return;
+    } else {
+        if (response.message) {
+            throw Error(response.message)
+        }
+        else {
+            throw Error('Unexpected Error')
+        }
+    }
+}
+
 export async function getLogin(email,password) {
-    let data = await fetch(`${URL}/users/login`, {
+    let response = await fetch(`${URL}/users/login`, {
         method: 'POST', 
         mode: 'cors', 
         cache: 'no-cache', 
@@ -14,55 +28,65 @@ export async function getLogin(email,password) {
         headers: {
             'Content-Type': 'application/json',
         },
-        body: {
+        body: JSON.stringify({
             email,
             password
-        }, 
+        }), 
     })
     .then(response => response.json());
 
-    return data
+    handleResponse(response)
+
+    return
 }
 
 export async function getPrefs() {
-    let data = await fetch(`${URL}/users/pref`, {
+    let response = await fetch(`${URL}/users/pref`, {
         method: 'GET',
         mode: 'cors',
         credentials: 'include',
     })
     .then(response => response.json());
 
+    let data = handleResponse(response)
+
     return data
 }
 
 export async function addPref(id) {
-    let data = await fetch(`${URL}/users/pref/${id}`, {
+    let response = await fetch(`${URL}/users/pref/${id}`, {
         method: 'POST',
         mode: 'cors',
         credentials: 'include',
     })
     .then(response => response.json());
 
-    return data
+    handleResponse(response)
+
+    return
 }
 
 export async function deletePref(id) {
-    let data = await fetch(`${URL}/users/pref/${id}`, {
+    let response = await fetch(`${URL}/users/pref/${id}`, {
         method: 'DELETE',
         mode: 'cors',
         credentials: 'include',
     })
     .then(response => response.json());
 
-    return data
+    handleResponse(response)
+
+    return
 }
 
 export async function getNearMe(lat,lng) {
-    let data = await fetch(`${URL}/shops/neame?lat=${lat}&lng=${lng}`, {
+    let response = await fetch(`${URL}/shops/neame?lat=${lat}&lng=${lng}`, {
         method: 'GET',
         mode: 'cors',
         credentials: 'include'
     })
+
+    let data = handleResponse(response)
 
     return data
 }
