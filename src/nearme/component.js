11d179ce8f4e65e1367dca7aef8447c57a430e5b
z3u2 <ilyassnasr@gmail.com>
2019-07-01 03:24:53 +0100

    Add basic nearme page logic

--- src/nearme/component.js
+++ src/nearme/component.js
@@ -1,10 +1,72 @@
 import React from 'react'
+import {getPositionPromise} from '../geo'
+import {getNearMe} from '../service'
 
-export default class Pref extends React.Component {
+export default class NearMe extends React.Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            location: null,
+            shops: null,
+            error: ''
+        }
+
+        this.getLocation = this.getLocation.bind(this)
+        this.updateShops = this.updateShops.bind(this)
+        this.updateLocation = this.updateLocation.bind(this)
+    }
+
+    async getLocation() {
+        let position = await getPositionPromise
+        let {
+            latitude : lat,
+            longitude : lng
+        } = position.coords
+        let shops = await getNearMe(lat,lng)
+        this.updateShops(shops)
+    }
+
+    updateShops(shops) {
+        this.setState({shops})
+    }
+
+    updateLocation(lat,lng) {
+        this.setState({location : {
+            lat,
+            lng
+        }})
+    }
+
+    componentDidMount() {
+        this.getLocation()
+        .catch(err => {
+            this.setState({error : err.message})
+        })
+    }
+
     render() {
+
+        const error = this.state.error
+        const shops = this.state.shops
+
         return (
             <div>
-                NearMe Component
+                { shops && <ul>
+                    {
+                        shops.map(shop => (
+                            <li key={shop._id}>
+                                Name : {shop.name}, Distance : {shop.dist}
+                            </li>
+                        ))
+                    }
+                </ul>}
+                {
+                    error && <p>
+                        {error}
+                    </p>
+                }
             </div>
         )
     }
