b335d3dc28418c4c4505d8d24213514349364ddb
z3u2 <ilyassnasr@gmail.com>
2019-07-01 02:06:30 +0100

    Add basic nearme page logic

--- src/nearme/component.js
+++ src/nearme/component.js
@@ -1,10 +1,38 @@
 import React from 'react'
+import { getPrefs } from '../service'
 
 export default class NearMe extends React.Component {
+
+    constructor(props){
+        super(props)
+
+        this.state = {
+            prefs : null
+        }
+    }
+
+    componentDidMount() {
+        getPrefs().then(
+            prefs => {
+                this.setState({prefs})
+            }
+        )
+    }
+
     render() {
+
+        const prefs = this.state.prefs
+
         return (
             <div>
-                NearMe Component
+                {prefs && 
+                <ul>
+                    {prefs.map(pref => {
+                        return (
+                            <li>{pref.name}</li>
+                        )
+                    })}
+                </ul>}
             </div>
         )
     }
