20dc44b59b153a0e2ac4aca4d64b96f2f6918004
z3u2 <ilyassnasr@gmail.com>
2019-10-12 16:30:04 +0100

    Change shop image position

--- src/components/shop.js
+++ src/components/shop.js
@@ -13,10 +13,10 @@ export default class Shop extends React.Component {
         return (
             <div className="card shop">
                 <div className="container">
+                    <img src="https://picsum.photos/200/300" className="card-img-top" alt={name + " picture"} />
                     <div className="card-title-wrapper">
                         <h5 className="card-title">{name}</h5>
                     </div>
-                    <img src="https://picsum.photos/200/300" className="card-img-top" alt={name + " picture"} />
                     <div className="card-body">
                         {this.props.children}
                     </div>
