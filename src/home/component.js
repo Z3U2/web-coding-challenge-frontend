5067b5481a4e58840d8fb0e5ac2273444003f0c6
z3u2 <ilyassnasr@gmail.com>
2019-10-14 22:09:41 +0100

    Add jumbotron styling and responsiveness

--- src/home/component.js
+++ src/home/component.js
@@ -6,9 +6,12 @@ export default class Home extends React.Component {
     render() {
         return (
             <div className="jumbotron">
-                <h1 className="display-4">Welcome to ShopList App !</h1>
-                <p className="lead">You can start browsing shops nearby.</p>
-                <hr className="my-4" />
+                <i class="fas fa-map-marker-alt"></i>
+                <div class="welcome-text">
+                    <h1 className="display-4">Welcome to ShopList App !</h1>
+                    <p className="lead">You can start browsing shops nearby.</p>
+                </div>
+                {/* <hr className="my-4" /> */}
                 <p>In a short while you'll be able to discover shops near you, 
                     and like them so we can save them for you !</p>
                 <Link className="btn btn-primary btn-lg" to='/nearme' role="button">Go !</Link>
